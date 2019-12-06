ef0f81be9f234bcabde9a76c5bb53ec0fbc43c4f
Dmitry Kanaev <dk@sbctech.ru>
2019-12-06 08:46:41 +0300

    interrupt example

---
+++ src/main/java/InterruptThreadExample.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class InterruptThreadExample {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Random random = new Random();
+                for (int i = 0; i < 1000_000_000; i++) {
+                    if (Thread.currentThread().isInterrupted()) {
+                        System.out.println("Thread was interrupted");
+                        break;
+                    }
+                    System.out.println(Math.sin(random.nextDouble()));
+                }
+            }
+        });
+
+        System.out.println("Starting thread");
+
+        thread.start();
+
+        Thread.sleep(1000);
+        thread.interrupt();
+
+        thread.join(); // join() после interrupt()!!!
+
+        System.out.println("Thread has finished");
+    }
+}
